4caa16ecb58b324b1bbcfe77ee2d158a0a836b96
Elwilliko <wchabla@est.ups.edu.ec>
2020-05-24 15:19:26 -0500

    creacion de login y colocar datos de php

--- public/controladores/crear_usuario.php
+++ public/controladores/crear_usuario.php
@@ -16,14 +16,11 @@ include '../../config/PHP.php';
 $cedula = isset($_POST["cedula"]) ? trim($_POST["cedula"]) : null;
 $nombres = isset($_POST["nombres"]) ? mb_strtoupper(trim($_POST["nombres"]), 'UTF-8') : null;
 $apellidos = isset($_POST["apellidos"]) ? mb_strtoupper(trim($_POST["apellidos"]), 'UTF-8') : null;
-$direccion = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["direccion"]), 'UTF-8') : null;
-$telefono = isset($_POST["telefono"]) ? trim($_POST["telefono"]): null;
+$roles = isset($_POST["roles"]) ? mb_strtoupper(trim($_POST["roles"]), 'UTF-8') : null;
 $correo = isset($_POST["correo"]) ? trim($_POST["correo"]): null;
-$fechaNacimiento = isset($_POST["fechaNacimiento"]) ? trim($_POST["fechaNacimiento"]): null;
 $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
 
-$sql = "INSERT INTO usuario VALUES (0, '$cedula', '$nombres', '$apellidos', '$direccion', '$telefono',
-'$correo', MD5('$contrasena'), '$fechaNacimiento', 'N', null, null)";
+$sql = "INSERT INTO usuario VALUES (0, '$cedula', '$nombres', '$apellidos', '$roles', '$correo', MD5('$contrasena'))";
 if ($conn->query($sql) === TRUE) {
     echo "<p>Se ha creado los datos personales correctamemte!!!</p>";
 } else {
